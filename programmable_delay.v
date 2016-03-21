c17b68250ef2fbd7e35d38405c378d174d691f7d
Nick Haynes <nickdavidhaynes@gmail.com>
2016-03-21 17:52:11 -0400

    commented code removed

--- programmable_delay.v
+++ programmable_delay.v
@@ -14,21 +14,6 @@ wire [N-1:0] mux_output /* synthesis keep */;
 wire [2*delta_delay*(N-1)-1:0] internal_wire /* synthesis keep */;
 
 // nested generate loops = ugly code
-/*
-assign mux_output[N-1] = code[N-1] & in;
-genvar i, j;
-generate
-	for (i = 0; i < (N-1); i = i+1)
-		begin: outer_loop
-			assign internal_wire[delta_delay*2*i] = ~((code[i] & in) | (~code[i] & mux_output[i+1]));
-			for (j = 1; j < 2*delta_delay; j = j+1)
-				begin: inner_loop
-					assign internal_wire[delta_delay*2*i+j] = ~internal_wire[delta_delay*2*i+j-1];
-				end
-			assign mux_output[i] = internal_wire[delta_delay*2*i+2*delta_delay-1];
-		end
-endgenerate
-*/
 assign mux_output[N-1] = in;
 genvar i, j;
 generate
