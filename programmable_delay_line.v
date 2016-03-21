4ec4da5c2050c9e7244d5fe1d97a9ee8af83c642
Nick Haynes <nickdavidhaynes@gmail.com>
2016-03-21 17:16:28 -0400

    switched from 1-hot to binary encoding of delays

--- programmable_delay_line.v
+++ programmable_delay_line.v
@@ -4,19 +4,20 @@ module programmable_delay_line(SW, SMA_CLKOUT);
 // for x in [0, N-1]
 parameter nominal_delay = 0;
 parameter delta_delay = 5;
-parameter N = 18;
+parameter N = 16;
+parameter log2_N = 4;
 
-input [N-1:0] SW;
+input [log2_N-1:0] SW;
 output SMA_CLKOUT;
 
-wire [N-1:0] code;
+wire [log2_N-1:0] code;
 wire pre_delayed /* synthesis keep */; 
 wire post_delayed /* synthesis keep */;
 
-assign code[N-1:0] = SW[N-1:0];
+assign code[log2_N-1:0] = SW[log2_N-1:0];
 assign pre_delayed = ~post_delayed;
 assign SMA_CLKOUT = post_delayed;
 
-programmable_delay #(.nominal_delay(nominal_delay), .delta_delay(delta_delay), .N(N)) (pre_delayed, code[N-1:0], post_delayed);
+programmable_delay #(.nominal_delay(nominal_delay), .delta_delay(delta_delay), .N(N), .log2_N(log2_N)) (pre_delayed, code[log2_N-1:0], post_delayed);
 
 endmodule
